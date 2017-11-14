1aebc9120ed3ce296dc840b007277c439c85d628
matshec <mmasakra@gmail.com>
2017-11-14 20:58:18 +0100

    histograms done

--- lab3/histogram.py
+++ lab3/histogram.py
@@ -54,7 +54,7 @@ lenaRGB = mpimg.imread("Histogram/lenaRGB.bmp")
 
 # zad 2
 
-# wywala błedy bo hold depricated
+# #wywala błedy bo hold depricated
 # ppl.hold(True)
 # v = ppl.hist(hist_im.flatten(),bins=256,range=(0,255))
 # hq = cum_hist(v)
@@ -62,7 +62,7 @@ lenaRGB = mpimg.imread("Histogram/lenaRGB.bmp")
 # ppl.hold(False)
 # ppl.show()
 
-# skum
+# #skum
 # v = ppl.hist(lena1.flatten(),bins=256,range=(0,255))
 # chs = cum_hist(v)
 # ln = Image.fromarray(lena1)
@@ -81,19 +81,61 @@ lenaG = lenaRGB[:, :, 1]
 lenaB = lenaRGB[:, :, 2]
 
 ppl.subplot(241)
-ppl.imshow(lenaR)
+ppl.imshow(lenaR,cmap="gray")
+ppl.title("Red")
 ppl.subplot(242)
 lrh = ppl.hist(lenaR.flatten(), bins=256, range=(0, 255))
 ppl.subplot(243)
-ppl.imshow(lenaG)
+ppl.imshow(lenaG,cmap="gray")
+ppl.title("green")
 ppl.subplot(244)
 lgh = ppl.hist(lenaG.flatten(), bins=256, range=(0, 255))
 ppl.subplot(245)
-ppl.imshow(lenaB)
+ppl.imshow(lenaB,cmap="gray")
+ppl.title("blue")
 ppl.subplot(246)
 lbh = ppl.hist(lenaB.flatten(), bins=256, range=(0, 255))
 
+cum_lrh = cum_hist(lrh)
+cum_lgh = cum_hist(lgh)
+cum_lbh = cum_hist(lbh)
 
+r_lut = get_lut_t(cum_lrh)
+g_lut = get_lut_t(cum_lgh)
+b_lut = get_lut_t(cum_lbh)
+
+
+rec_r = Image.fromarray(lenaR).point(r_lut)
+rec_g = Image.fromarray(lenaG).point(g_lut)
+rec_b = Image.fromarray(lenaB).point(b_lut)
+
+ret = Image.merge("RGB",(rec_r,rec_g,rec_b))
+ret.show()
+
+# #HSV
+ppl.figure(2)
+HSV_lena = Image.fromarray(lenaRGB,mode="HSV")
+np_HSV = np.asarray(HSV_lena)
+lenaH = np_HSV[:, :, 0]
+lenaS = np_HSV[:, :, 1]
+lenaV = np_HSV[:, :, 2]
+
+ppl.subplot(241)
+H_hist = ppl.hist(lenaH.flatten(), bins=256, range=(0, 255))
+ppl.subplot(242)
+S_hist = ppl.hist(lenaS.flatten(), bins=256, range=(0, 255))
+ppl.subplot(243)
+V_hist = ppl.hist(lenaV.flatten(), bins=256, range=(0, 255))
+
+cum_V = cum_hist(V_hist)
+lut_v = get_lut_t(cum_V)
+rec_V = Image.fromarray(lenaV).point(lut_v)
+
+rec_lenaHSV = Image.merge("HSV",(Image.fromarray(lenaH),Image.fromarray(lenaS),rec_V))
+ppl.figure(3)
+ppl.imshow(rec_lenaHSV)
+ppl.figure(4)
+ppl.imshow(HSV_lena)
 
 
 ppl.show()
